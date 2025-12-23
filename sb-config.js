5d36aaa7e1ff3a6405c9f664b73003a8fe5b5987
krmsaed3-lgtm <krmsaed3@gmail.com>
2025-12-23 06:22:08 +0200

    Add files via upload

--- sb-config.js
+++ sb-config.js
@@ -9,17 +9,13 @@
   'use strict';
 
   // These values are provided by the user. Do not modify them here.
-  var url = 'https://daaxfvepnujsuftzcfcv.supabase.co;
+  var url = 'https://daaxfvepnujsuftzcfcv.supabase.co';
   var anonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRhYXhmdmVwbnVqc3VmdHpjZmN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY0NTUxMzMsImV4cCI6MjA4MjAzMTEzM30.WjpjKmK2j7omDIH2n_Q7gzC7Ei-KinARMF2FlUj4qyA';
 
   // Build an object with helper functions for constructing API requests.
   var SB_CONFIG = {
     url: url,
     anonKey: anonKey,
-    /**
-     * Returns common headers for Supabase REST requests. The
-     * Authorization header uses the anon key by default.
-     */
     headers: function () {
       return {
         'apikey': anonKey,
